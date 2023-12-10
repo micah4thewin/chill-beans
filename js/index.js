ee2271dc4bbe3bae6623793c23e9325fe4e1488c
micah4thewin <contact@micahlevason.com>
2023-12-09 21:57:25 -0800

    new

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 
   // Function to create card HTML
-  const createCardHtml = (item, index) =>
+  const createCardHtml = (item, index, type) =>
     `<div class="col-12 col-md-6 col-lg-4 mb-4">
       <div class="card rounded">
         <img src="${item.photo}" class="card-img-top" alt="${item.name}" data-aos="zoom-in">
@@ -29,13 +29,13 @@ document.addEventListener("DOMContentLoaded", function() {
               ${item.sizes
                 .map(
                   (size, sizeIndex) =>
-                    `<button id="size-btn-${index}-${sizeIndex}" type="button" class="btn btn-secondary" data-item='${JSON.stringify(
+                    `<button id="size-btn-${type}-${index}-${sizeIndex}" type="button" class="btn btn-secondary" data-item='${JSON.stringify(
                       item
                     )}' data-size-index="${sizeIndex}">${size.size}</button>`
                 )
                 .join("")}
             </div>
-            <p id="price-${index}" class="card-text"><small class="text-muted fs-3">${item.sizes[0].price}</small></p>
+            <p id="price-${type}-${index}" class="card-text"><small class="text-muted fs-3">${item.sizes[0].price}</small></p>
           </div>
         </div>
       </div>
@@ -54,41 +54,45 @@ document.addEventListener("DOMContentLoaded", function() {
     let htmlContent = '';
 
     [foodData, slushyData, coffeeData].forEach((data) => {
-      data.forEach((item) => {
-        let currentIndex;
-        if (data === foodData) {
-          htmlContent += createCardHtml(item, foodIndex);
-          currentIndex = foodIndex;
-          foodIndex++;
-        } else if (data === slushyData) {
-          htmlContent += createCardHtml(item, slushyIndex);
-          currentIndex = slushyIndex;
-          slushyIndex++;
-        } else {
-          htmlContent += createCardHtml(item, coffeeIndex);
-          currentIndex = coffeeIndex;
-          coffeeIndex++;
-        }
-      });
-    });
+       data.forEach((item) => {
+         let currentIndex;
+         let type;
+         if (data === foodData) {
+           htmlContent += createCardHtml(item, foodIndex, 'food');
+           currentIndex = foodIndex;
+           type = 'food';
+           foodIndex++;
+         } else if (data === slushyData) {
+           htmlContent += createCardHtml(item, slushyIndex, 'slushy');
+           currentIndex = slushyIndex;
+           type = 'slushy';
+           slushyIndex++;
+         } else {
+           htmlContent += createCardHtml(item, coffeeIndex, 'coffee');
+           currentIndex = coffeeIndex;
+           type = 'coffee';
+           coffeeIndex++;
+         }
+       });
+     });
 
-    cardSection.innerHTML = htmlContent;
+     cardSection.innerHTML = htmlContent;
 
-    cardSection.addEventListener("click", (event) => {
-      if (event.target.tagName === "BUTTON") {
-        const [btnType, currentIndex, sizeIndex] = event.target.id.split("-");
-        const item = JSON.parse(event.target.getAttribute("data-item"));
-        const newSizeIndex = parseInt(event.target.getAttribute("data-size-index"));
-        document.getElementById(`price-${currentIndex}`).innerHTML = `<small class="text-muted fs-3">${item.sizes[newSizeIndex].price}</small>`;
-      }
-    });
+     cardSection.addEventListener("click", (event) => {
+       if (event.target.tagName === "BUTTON") {
+         const [btnType, itemType, currentIndex, sizeIndex] = event.target.id.split("-");
+         const item = JSON.parse(event.target.getAttribute("data-item"));
+         const newSizeIndex = parseInt(event.target.getAttribute("data-size-index"));
+         document.getElementById(`price-${itemType}-${currentIndex}`).innerHTML = `<small class="text-muted fs-3">${item.sizes[newSizeIndex].price}</small>`;
+       }
+     });
 
-    AOS.init();
-  };
+     AOS.init();
+   });
 
-  // Call function to populate card section
-  populateCardSection();
+   // Call function to populate card section
+   populateCardSection();
 
-  console.log('4');
+  console.log('5');
 
 });
