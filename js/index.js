636fe94d7fe491c5e0adda3b3cf1104e2abc3b3b
micah4thewin <contact@micahlevason.com>
2023-12-09 21:11:35 -0800

    4

--- js/index.js
+++ js/index.js
@@ -17,23 +17,29 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 
   // Function to create card HTML
-  const createCardHtml = (item, index) => `
-    <div class="col-12 col-md-6 col-lg-4 mb-4">
+  const createCardHtml = (item, index) =>
+    `<div class="col-12 col-md-6 col-lg-4 mb-4">
       <div class="card rounded">
         <img src="${item.photo}" class="card-img-top" alt="${item.name}" data-aos="zoom-in">
-        <div class="card-body" >
+        <div class="card-body">
           <h5 class="card-title fs-3">${item.name}</h5>
           <p class="card-text">${item.description}</p>
           <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group" role="group">
-              ${item.sizes.map((size, sizeIndex) => `<button id="size-btn-${index}-${sizeIndex}" type="button" class="btn btn-secondary" data-item='${JSON.stringify(item)}'>${size.size}</button>`).join('')}
+              ${item.sizes
+                .map(
+                  (size, sizeIndex) =>
+                    `<button id="size-btn-${index}-${sizeIndex}" type="button" class="btn btn-secondary" data-item='${JSON.stringify(
+                      item
+                    )}' data-size-index="${sizeIndex}">${size.size}</button>`
+                )
+                .join("")}
             </div>
-            <p id="price-${index}" class="card-text"><small class="text-muted fs-3">$${item.sizes[0].price}</small></p>
+            <p id="price-${index}" class="card-text"><small class="text-muted fs-3">${item.sizes[0].price}</small></p>
           </div>
         </div>
       </div>
-    </div>
-  `;
+    </div>`;
 
   // Populate card section
   const populateCardSection = async () => {
@@ -68,11 +74,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     cardSection.innerHTML = htmlContent;
 
-    cardSection.addEventListener('click', (event) => {
-      if (event.target.tagName === 'BUTTON') {
-        const [btnType, currentIndex, sizeIndex] = event.target.id.split('-');
-        const item = JSON.parse(event.target.getAttribute('data-item'));
-        document.getElementById(`price-${currentIndex}`).innerHTML = `<small class="text-muted fs-3">$${item.sizes[sizeIndex].price}</small>`;
+    cardSection.addEventListener("click", (event) => {
+      if (event.target.tagName === "BUTTON") {
+        const [btnType, currentIndex, sizeIndex] = event.target.id.split("-");
+        const item = JSON.parse(event.target.getAttribute("data-item"));
+        const newSizeIndex = parseInt(event.target.getAttribute("data-size-index"));
+        document.getElementById(`price-${currentIndex}`).innerHTML = `<small class="text-muted fs-3">${item.sizes[newSizeIndex].price}</small>`;
       }
     });
 
@@ -82,6 +89,6 @@ document.addEventListener("DOMContentLoaded", function() {
   // Call function to populate card section
   populateCardSection();
 
-  console.log('3');
+  console.log('4');
 
 });
