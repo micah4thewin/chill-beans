ab5d741b207b3968047b01ba3173ef5e04d59fec
micah4thewin <contact@micahlevason.com>
2023-12-12 09:26:45 -0800

    new

--- js/index.js
+++ js/index.js
@@ -10,20 +10,6 @@ var typed = new Typed('#element', {
 
 document.addEventListener("DOMContentLoaded", async function () {
 
-  // Initialize Typed.js
-const typedElement = document.querySelector('#typed');
-  if (typedElement) {
-    const typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    loop: true,
-    loopCount: Infinity,
-    typeSpeed: 25,
-    backSpeed: 15,
-    showCursor: false,
-    cursorChar: '|',
-    autoInsertCss: true,
-  });
-  }
   const fetchJsonData = async (filename) => {
     const response = await fetch(filename);
     const data = await response.json();
@@ -45,12 +31,12 @@ const typedElement = document.querySelector('#typed');
     });
   };
   const createCardHtml = (item, index, type) =>
-    `<div class="col-12 col-md-6 col-lg-4 mb-4">
-      <div class="card rounded">
-        <img src="${item.photo}" class="card-img-top" alt="${item.name}" data-aos="zoom-in">
+    `<div class="col-12 col-md-6 col-lg-4 mb-4" data-aos="fade-up">
+      <div class="card rounded" data-aos="zoom-in">
+        <img src="${item.photo}" class="card-img-top" alt="${item.name}">
         <div class="card-body">
-          <h5 class="card-title fs-3">${item.name}</h5>
-          <p class="card-text">${item.description}</p>
+          <h5 class="card-title fs-3" data-aos="fade-up">${item.name}</h5>
+          <p class="card-text" data-aos="fade-up">${item.description}</p>
           <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group" role="group">
               ${item.sizes
@@ -58,15 +44,16 @@ const typedElement = document.querySelector('#typed');
                   (size, sizeIndex) =>
                     `<button id="size-btn-${type}-${index}-${sizeIndex}" type="button" class="btn btn-secondary" data-item='${JSON.stringify(
                       item
-                    )}' data-size-index="${sizeIndex}">${size.size}</button>`
+                    )}' data-size-index="${sizeIndex}" data-aos="fade-up">${size.size}</button>`
                 )
                 .join("")}
             </div>
-            <p id="price-${type}-${index}" class="card-text"><small class="text-muted fs-3">$${item.sizes[0].price}</small></p>
+            <p id="price-${type}-${index}" class="card-text" data-aos="fade-up"><small class="text-muted fs-3">$${item.sizes[0].price}</small></p>
           </div>
         </div>
       </div>
     </div>`;
+
 
   // Populate card section
   const populateCardSection = async () => {
@@ -112,6 +99,6 @@ const typedElement = document.querySelector('#typed');
   // Call function to populate card section
   await populateCardSection();
   AOS.init();
-  console.log('12');
+  console.log('13');
 
 });
